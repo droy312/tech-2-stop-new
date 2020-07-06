0865d50f29f2fe315d1dd89d445208384cf3dbff
Dlhritiman Roy <dhritiman.roy312@gmail.com>
2020-07-06 20:08:48 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:tech_2_stop_new/screens/home/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tech 2 Stop',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
